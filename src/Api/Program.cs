9a759adea1d802503da493114a7436e9125ffa01
Jim Johnson <email_programmer_jim@yahoo.com>
2026-01-15 21:52:54 -0600

    Call extension method to bootstrap base web service with its interface.

--- src/Api/Program.cs
+++ src/Api/Program.cs
@@ -1,3 +1,5 @@
+using FoodStorageApi.Infrastructure;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -5,6 +7,9 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// Add Infrastructure services
+builder.Services.AddInfrastructure();
 
 var app = builder.Build();
 
